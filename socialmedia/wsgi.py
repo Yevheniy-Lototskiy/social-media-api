d376e951247be9f29028416687a0a3951dcaa8c8
Eugene <eugene.lot77@gmail.com>
2023-04-24 12:08:30 +0300

    Implemented user app & added settings

--- socialmedia/wsgi.py
+++ socialmedia/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for socialmedia project.
+WSGI config for socialmedia1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia1.settings')
 
 application = get_wsgi_application()
