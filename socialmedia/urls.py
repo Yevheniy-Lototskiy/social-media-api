1c241b9f0c016cdb6e1ad66e8600b5a9e3eba817
Eugene <eugene.lot77@gmail.com>
2023-04-24 12:29:01 +0300

    Implemented logic for login/logout

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("api/user/", include("user.urls", namespace="user")),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token-auth/', obtain_auth_token, name='api_token_auth'),
+    # path('api/', include('api.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
